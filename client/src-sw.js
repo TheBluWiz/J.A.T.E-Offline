fd53f40a59de8f137b6fe085d4af33931b148338
Jamey Wicklund <thebluwiz@icloud.com>
2023-02-23 12:31:09 -0800

    Adds indexDB functionality

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,9 +1,32 @@
-const { StaleWhileRevalidate } = require('workbox-strategies');
+const { StaleWhileRevalidate, CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { warmStrategyCache } = require('workbox-recipes');
+const { ExpirationPlugin } = require('workbox-expiration');
 
 precacheAndRoute(self.__WB_MANIFEST);
+
+const pageCache = new CacheFirst({
+  cacheName: "page-cache",
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200]
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 60 * 60 * 24 * 30
+    })
+  ]
+})
+
+warmStrategyCache({
+  urls: ["/index.html", "/"],
+  strategy: pageCache
+})
+
+registerRoute(
+  ({ request }) => request.mode === "navigate", pageCache
+)
 
 registerRoute(
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
